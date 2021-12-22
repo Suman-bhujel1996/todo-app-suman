33d651ec447f061f875fb06da395282cd4bb6908
Suman-bhujel1996 <suman.bhujel2468@gmail.com>
2021-12-22 10:54:33 +0545

    models added

--- app/src/main/java/com/example/todo_app/MainActivity.java
+++ app/src/main/java/com/example/todo_app/MainActivity.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    Repository repository;
+    List<Todo> todos;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
